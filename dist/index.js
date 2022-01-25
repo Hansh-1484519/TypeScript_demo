0ade4d23fa661ba64fe061b11c9642881ad209a7
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-25 14:36:10 +0530

    uploaded initial required files

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Basic type
+let id = 344;
+let userName = "hansh";
+let flag = true;
+let x = "hello there";
+let arr = [2, 23, 4, 3, 3];
+let idArray = [3, true, "string", ["string array"]];
+let person = [1, "brad", true];
+console.log(id);
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Basic type
+
+let id : number = 344;
+let userName : String = "hansh";
+let flag : boolean = true;
+let x : any = "hello there";
+let arr : number[] = [2 ,23 , 4 , 3, 3];
+
+let idArray : any[] = [ 3 , true , "string" , [ "string array" ]];
+let person : [number , string , boolean] = [1 , "brad" , true];
+
+console.log( id );
