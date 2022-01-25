99abed543e18ce8b49f41cb71608e4b43c08a76b
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-25 14:51:54 +0530

    added more datatype to index.ts file

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,17 @@
 let id = 344;
 let userName = "hansh";
 let flag = true;
+//any datatype
 let x = "hello there";
+//array
 let arr = [2, 23, 4, 3, 3];
 let idArray = [3, true, "string", ["string array"]];
+//tuple
 let person = [1, "brad", true];
+//tuple array
+let employee = [
+    [2, "hello"],
+    [3, "ddkso"],
+    [4, "good night"]
+];
 console.log(id);
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,19 @@
 let id : number = 344;
 let userName : String = "hansh";
 let flag : boolean = true;
+//any datatype
 let x : any = "hello there";
-let arr : number[] = [2 ,23 , 4 , 3, 3];
 
+//array
+let arr : number[] = [2 ,23 , 4 , 3, 3];
 let idArray : any[] = [ 3 , true , "string" , [ "string array" ]];
+//tuple
 let person : [number , string , boolean] = [1 , "brad" , true];
+
+//tuple array
+let employee : [number , string][] = [ 
+    [2 , "hello"],
+    [ 3 , "ddkso"],
+    [ 4 , "good night"] ];
 
 console.log( id );
